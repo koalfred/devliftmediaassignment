ce2a4518b0950b0fb32c59ecb3264bf2e39423f5
Alfred Ko <57697525+koalfred@users.noreply.github.com>
2021-06-11 14:55:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var nav = document.getElementById("nav");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+}
